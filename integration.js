4585b246bc92e095a1118ec50ba1af552cea30a7
Guilloux Nicolas <novares.x@gmail.com>
2018-05-06 22:46:33 +0100

    Change of strategy
    A huge strange of strategy: instead of simulate click events on the UI button, the plugin will directly interact with the JSONRPC server. Now, the user can select whatever webUI he wants. Moreover, the data shown are more accurate.
    However, it consumes a lot more network ressources. The updateInteval at the top is meant to change the update interval

--- integration.js
+++ integration.js
@@ -1,160 +1,171 @@
+// Parameter
+var updateInterval = 1;
+
+// Global variables
+var baseUrl     = window.location.protocol + "//" + window.location.host;
+var id          = 0;
+var prevTime    = 0;
+
+var songTitle   = "",
+    artistName  = "",
+    albumTitle  = "",
+    artUrl      = "",
+    duration    = 0,
+    position    = 0,
+    volume      = 0;
+    playbackStatus = mellowplayer.PlaybackStatus.STOPPED;
+
+/**
+ * Contact the Kodi server
+ *
+ * @param string   Method
+ * @param array    Array of parameters
+ * @param function Callback function on success
+ */
+function sendQuery(method, params, onComplete) {
+    var query = {
+        "jsonrpc": "2.0",
+        "id": id,
+        "method": method,
+        "params": params
+    };
+    id++;
+
+    var xhttp = new XMLHttpRequest();
+
+    if(onComplete) {
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                onComplete(this.responseText);
+            }
+        };
+    }
+
+    xhttp.open("GET", baseUrl + "/jsonrpc?request=" + JSON.stringify(query), true);
+    xhttp.send();
+}
+
+/**
+ * Update function
+ */
 function update() {
+
+    // Check if an update should be made
+    if( new Date()/1000 - prevTime > updateInterval ) {
+        prevTime = new Date()/1000;
+
+        // Get song info
+        var listProperties = ["title", "album", "artist", "duration", "thumbnail"];
+        sendQuery("Player.GetItem", {"playerid": 0, "properties": listProperties}, function(data) {
+
+            // Parse the json
+            var json = JSON.parse(data).result;
+
+            // Set the properties
+            if(json.item.type != "unknown") {
+                songTitle  = json.item.title;
+                albumTitle = json.item.album;
+                artistName = json.item.artist[0];
+                duration   = json.item.duration;
+                artUrl     = baseUrl + "/image/" + encodeURI(json.item.thumbnail);
+
+            } else {
+                songTitle  = "";
+                albumTitle = "";
+                artistName = "";
+                duration   = 0;
+                artUrl     = "";
+            }
+
+        });
+
+        // Get volume
+        sendQuery("Application.GetProperties", {"properties": ["volume"]}, function(data) {
+
+            // Parse the json
+            json = JSON.parse(data).result;
+
+            // Set the volume
+            volume = json.volume/100;
+        });
+
+        // Get position and status
+        sendQuery("Player.GetProperties", {"playerid":0, "properties": ["time", "speed"] }, function(data) {
+
+            // Parse the json
+            json = JSON.parse(data).result;
+
+            // Set the position
+            position = json.time.hours * 3600 + json.time.minutes * 60 + json.time.seconds;
+
+            // Set the status
+            switch(json.speed) {
+                case 1:
+                    playbackStatus = mellowplayer.PlaybackStatus.PLAYING;
+                    break;
+
+                default:
+                    if( songTitle != "" )
+                        playbackStatus = mellowplayer.PlaybackStatus.PAUSED;
+
+                    else
+                        playbackStatus = mellowplayer.PlaybackStatus.STOPPED;
+            }
+        });
+    }
+
     return {
-        "playbackStatus": getState(),
-        "canSeek": false,
+        "playbackStatus": playbackStatus,
+        "volume": volume,
+        "duration": duration,
+        "position": position,
+        "songId": getHashCode(songTitle),
+        "songTitle": songTitle,
+        "artistName": artistName,
+        "albumTitle": albumTitle,
+        "artUrl": artUrl,
+
+        "canSeek": true,
         "canGoNext": true,
         "canGoPrevious": true,
         "canAddToFavorites": false,
-        "volume": 0.4,
-        "duration": getDuration(),
-        "position": getPosition(),
-        "songId": 0,
-        "songTitle": getTitle(),
-        "artistName": getArtistName(),
-        "albumTitle": "",
-        "artUrl": getArtwork(),
         "isFavorite": false
     };
 }
 
 function play() {
-    eventFire(document.getElementsByClassName('control-play')[0], 'click');
+    sendQuery('Player.PlayPause', {"playerid": 0});
 }
 
 function pause() {
-    eventFire(document.getElementsByClassName('control-play')[0], 'click');
+    sendQuery('Player.PlayPause', {"playerid": 0});
 }
 
 function goNext() {
-    eventFire(document.getElementsByClassName('control-next')[0], 'click');
+    sendQuery('Player.GoTo', {"playerid": 0, "to": "next"});
 }
 
 function goPrevious() {
-    eventFire(document.getElementsByClassName('control-prev')[0], 'click');
+    sendQuery('Player.GoTo', {"playerid": 0, "to": "previous"});
 }
 
-function getTitle() {
-    var element = document.getElementsByClassName('playing-title')[0];
-
-    if(element)
-        return element.innerText;
-
-    return "";
+// Volume float 0-1
+function setVolume(newVolume) {
+    volume = newVolume;
+    sendQuery('Application.SetVolume', {'volume': Math.floor(newVolume*100)});
 }
 
-function getArtistName() {
-    var element = document.getElementsByClassName('playing-subtitle')[0];
-
-    if(element)
-        return element.innerText;
-
-    return "";
-}
-
-function getVolume() {
-    var field = document.getElementsByClassName('volume')[0].getElementsByClassName('noUi-origin')[0];
-
-    if(field)
-        return parseFloat( field.style.left ) / 100;
-
-    return 0;
-}
-
-function getDuration() {
-    var field = document.getElementsByClassName('playing-time-duration')[0];
-
-    if(field) {
-        var array = field.innerText.split(':');
-
-        if(array.length == 1)
-            return parseInt(array[0]);
-
-        else if(array.length == 2)
-            return parseInt(array[0])*60 + parseInt(array[1]);
-
-        else
-            return parseInt(array[0])*3600 + parseInt(array[1])*60 + parseInt(array[2]);
-    }
-
-    return 0;
-}
-
-function getPosition() {
-    var field = document.getElementsByClassName('playing-time-current')[0];
-
-    if(field) {
-        var array = field.innerText.split(':');
-
-        if(array.length == 1)
-            return parseInt(array[0]);
-
-        else if(array.length == 2)
-            return parseInt(array[0])*60 + parseInt(array[1]);
-
-        else
-            return parseInt(array[0])*3600 + parseInt(array[1])*60 + parseInt(array[2]);
-    }
-
-    return 0;
-}
-
-function getArtwork() {
-    var baseUrl = window.location.href.split('#')[0];
-    var field = document.getElementsByClassName('playing-thumb')[0];
-
-    return baseUrl + field.style['background-image'].replace('url(\"', '').replace('\")', '');
-}
-
-// volume float 0-1
-function setVolume(volume) {
-    var data = [
-        {
-            "id": 1000,
-            "jsonrpc": 2.0,
-            "method": "Application.setVolume",
-            "params": [
-                volume*100
-            ]
+// Position in seconds
+function seekToPosition(newPosition) {
+    position = newPosition;
+    sendQuery('Player.Seek', {
+        "playerid": 0, "value": {
+            "hours": Math.floor(newPosition/3600) %60,
+            "milliseconds": 0,
+            "minutes": Math.floor(newPosition/60) %60,
+            "seconds": position %60
         }
-    ];
-
-    $.post("http://192.168.0.25:8080/jsonrpc?Application.SetVolume", data, function(data, status){
-        console.log("Data: " + data + "\nStatus: " + status);
     });
-}
-
-// position in seconds
-function seekToPosition(position) {
-    //document.getElementsByClassName('playing-progress')[0].getElementsByClassName('noUi-origin')[0].
-}
-
-/**
- * Get the playing state
- */
-function getState() {
-    // Playing
-    if( document.getElementsByClassName('kodi-playing')[0] )
-        return mellowplayer.PlaybackStatus.PLAYING;
-
-    // Paused
-    if( document.getElementsByClassName('kodi-paused')[0] )
-        return mellowplayer.PlaybackStatus.PAUSED;
-
-    // Stopped
-    return mellowplayer.PlaybackStatus.STOPPED;
-}
-
-/**
- * Send an event
- */
-function eventFire(el, etype){
-    if (el.fireEvent) {
-        el.fireEvent('on' + etype);
-    } else {
-        var evObj = document.createEvent('Events');
-        evObj.initEvent(etype, true, false);
-        el.dispatchEvent(evObj);
-    }
 }
 
 function addToFavorites() {}
